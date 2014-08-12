2b4f95f34cbd48de2d5020bbb25bc7241b1ba1a5
Junggu Lee <hyde1117@naver.com>
2014-08-12 23:15:30 +0900

    complete front_times

--- warmup-2.py
+++ warmup-2.py
@@ -3,11 +3,19 @@ import unittest
 def string_times(str, n):
 	return str * n
 
+def front_times(str, n):	
+	return str[:3] * n
+
 class TestWarmUp2(unittest.TestCase):
 	def test_string_times(self):
 		self.assertEqual(string_times('Hi', 2), 'HiHi')
 		self.assertEqual(string_times('Hi', 3), 'HiHiHi')
 		self.assertEqual(string_times('Hi', 1), 'Hi')
 
+	def test_front_times(self):
+		self.assertEqual(front_times('Chocolate', 2), 'ChoCho')
+		self.assertEqual(front_times('Chocolate', 3), 'ChoChoCho')
+		self.assertEqual(front_times('Abc', 3), 'AbcAbcAbc')
+		
 if __name__ == "__main__":
 	unittest.main()
