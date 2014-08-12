27b8b53b38962255d78efd70ea68eb224969e818
Junggu Lee <hyde1117@naver.com>
2014-08-12 23:19:31 +0900

    complete string_bits

--- warmup-2.py
+++ warmup-2.py
@@ -6,6 +6,9 @@ def string_times(str, n):
 def front_times(str, n):	
 	return str[:3] * n
 
+def string_bits(str):
+	return str[::2]
+
 class TestWarmUp2(unittest.TestCase):
 	def test_string_times(self):
 		self.assertEqual(string_times('Hi', 2), 'HiHi')
@@ -16,6 +19,11 @@ class TestWarmUp2(unittest.TestCase):
 		self.assertEqual(front_times('Chocolate', 2), 'ChoCho')
 		self.assertEqual(front_times('Chocolate', 3), 'ChoChoCho')
 		self.assertEqual(front_times('Abc', 3), 'AbcAbcAbc')
+
+	def test_string_bits(self):
+		self.assertEqual(string_bits('Hello'), 'Hlo')
+		self.assertEqual(string_bits('Hi'), 'H')
+		self.assertEqual(string_bits('Heeololeo'), 'Hello')
 		
 if __name__ == "__main__":
 	unittest.main()
