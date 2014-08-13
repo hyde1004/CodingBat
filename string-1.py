0c2b7522d692f31534c538c739f219ad5c6e9353
이정구 <hyde1004@gmail.com>
2014-08-13 13:06:29 +0900

    Complete make_abba

--- string-1.py
+++ string-1.py
@@ -3,12 +3,19 @@ import unittest
 def hello_name(name):
 	return 'Hello ' + name + '!'
 
+def make_abba(a, b):
+	return a + b + b + a
+
 class TestString1(unittest.TestCase):
 	def test_hello_name(self):
 		self.assertEqual(hello_name('Bob'), 'Hello Bob!')
 		self.assertEqual(hello_name('Alice'), 'Hello Alice!')
 		self.assertEqual(hello_name('X'), 'Hello X!')
 
-		pass
+	def test_make_abba(self):
+		self.assertEqual(make_abba('Hi', 'Bye'), 'HiByeByeHi')
+		self.assertEqual(make_abba('Yo', 'Alice'), 'YoAliceAliceYo')
+		self.assertEqual(make_abba('What', 'Up'), 'WhatUpUpWhat')
+
 if __name__ == "__main__":
 	unittest.main()
