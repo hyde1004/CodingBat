8e334b0bdb387d1c3b286d6403dfd3141a839e2e
Junggu Lee <hyde1117@naver.com>
2014-08-22 23:04:03 +0900

    Complete common_end

--- list-1.py
+++ list-1.py
@@ -9,6 +9,9 @@ def same_first_last(nums):
 def make_pi():
 	return [3, 1, 4]
 	
+def common_end(a, b):
+	return (a[0] == b[0] or a[-1] == b[-1])
+
 class TestList1(unittest.TestCase):
 	def test_first_last6(self):
 		self.assertEqual(first_last6([1, 2, 6]), True)
@@ -20,6 +23,11 @@ class TestList1(unittest.TestCase):
 
 	def test_make_pi(self):
 		self.assertEqual(make_pi(), [3, 1, 4])
+
+	def test_common_end(self):
+		self.assertEqual(common_end([1, 2, 3], [7, 3]), True)
+		self.assertEqual(common_end([1, 2, 3], [7, 3, 2]), False)
+		self.assertEqual(common_end([1, 2, 3], [1, 3]), True)
 
 if __name__ == "__main__":
 	unittest.main()
